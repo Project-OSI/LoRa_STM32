034ef78cda05f64f9fa8eaa6334522ebcd638412
Project-OSI <philippe.hess@opensmartirrigation.org>
2026-08-18 01:50:55 +0200

    test: make Chameleon build guard cwd-independent

--- tests/test_chameleon_build_script.c
+++ tests/test_chameleon_build_script.c
@@ -19,6 +19,23 @@ static char *read_file(const char *path)
     return data;
 }
 
+static char *read_from_candidates(const char *const *paths, size_t path_count)
+{
+    size_t index;
+
+    for (index = 0U; index < path_count; ++index) {
+        FILE *file = fopen(paths[index], "rb");
+
+        if (file != NULL) {
+            fclose(file);
+            return read_file(paths[index]);
+        }
+    }
+
+    fprintf(stderr, "FAIL could not locate build input\n");
+    exit(1);
+}
+
 static void require(const char *data, const char *text)
 {
     if (strstr(data, text) == 0) {
@@ -37,12 +54,32 @@ static void forbid(const char *data, const char *text)
 
 int main(void)
 {
-    char *script = read_file("../build/build.sh");
-    char *flags = read_file("../build/cflags.rsp");
-    char *ignore = read_file("../build/.gitignore");
-    char *hardware = read_file(
+    static const char *const script_paths[] = {
+        "build/build.sh",
+        "../build/build.sh"
+    };
+    static const char *const flag_paths[] = {
+        "build/cflags.rsp",
+        "../build/cflags.rsp"
+    };
+    static const char *const ignore_paths[] = {
+        "build/.gitignore",
+        "../build/.gitignore"
+    };
+    static const char *const hardware_paths[] = {
+        "STM32CubeExpansion_LRWAN/Projects/Multi/Applications/LoRa/"
+        "DRAGINO-LRWAN(AT)/src/chameleon_lsn50_hw.c",
         "../STM32CubeExpansion_LRWAN/Projects/Multi/Applications/LoRa/"
-        "DRAGINO-LRWAN(AT)/src/chameleon_lsn50_hw.c");
+        "DRAGINO-LRWAN(AT)/src/chameleon_lsn50_hw.c"
+    };
+    char *script = read_from_candidates(script_paths,
+        sizeof(script_paths) / sizeof(script_paths[0]));
+    char *flags = read_from_candidates(flag_paths,
+        sizeof(flag_paths) / sizeof(flag_paths[0]));
+    char *ignore = read_from_candidates(ignore_paths,
+        sizeof(ignore_paths) / sizeof(ignore_paths[0]));
+    char *hardware = read_from_candidates(hardware_paths,
+        sizeof(hardware_paths) / sizeof(hardware_paths[0]));
 
     if (strstr(script, "/home/") || strstr(flags, "/home/")) {
         fprintf(stderr, "FAIL build inputs contain absolute checkout paths\n");
